d87478927a70d13dcdb7c2b0a16f7ac4639d39bd
Roman Rudenko <roman@mobify.com>
2012-06-07 03:52:11 -0700

    Attempted speeding up element disabling

--- api/externals.js
+++ api/externals.js
@@ -18,7 +18,7 @@ var keys = function(obj) { return $.map(obj, function(val, key) { return key }) 
       , link:   ['href']
       , style:  ['media']
     }
-  , affectedTagRe = new RegExp('<(' + keys(disablingMap).join('|') + ')([\\s\\S]*?)>', 'gi')
+  , affectedTagRe = /<!--[\s\S]*?-->|<(script)([\s\S]*?)>([\s\S]*?<\/script)|<(img|iframe|link|style)([\s\S]*?)>()/gi
   , attributeDisablingRes = {}
   , attributesToEnable = {}
   , attributeEnablingRe;
@@ -38,35 +38,19 @@ $.each(disablingMap, function(tagName, targetAttributes) {
 
 attributeEnablingRe = new RegExp('\\sx-(' + keys(attributesToEnable).join('|') + ')', 'gi');
 
+function disableAttributes(whole, tagName, openingTag, rest) {
+    if (!tagName) return whole;
 
-function disableAttributes(whole, tagName, tail) {
     tagName = tagName.toLowerCase();
     return result = '<' + tagName + (tagDisablers[tagName] || '')
-        + tail.replace(attributeDisablingRes[tagName], ' x-$1') + '>';
+        + openingTag.replace(attributeDisablingRes[tagName], ' x-$1') + '>' + rest;
 }
     
 // Returns a string with all external attributes disabled.
 // Includes special handling for resources referenced in scripts and inside
 // comments.
-Mobify.html.disable = function(htmlStr) {            
-    var splitRe = /(<!--[\s\S]*?-->)|(?=<\/script)/i
-      , tokens = htmlStr.split(splitRe)
-      , ret = tokens.map(function(fragment) {
-            var parsed
-
-            // Fragment may be empty or just a comment, no need to escape those.
-            if (!fragment) return '';
-            if (/^<!--/.test(fragment)) return fragment;
-
-            // Disable before and the <script> itself.
-            // parsed = [before, <script>, script contents]
-            parsed = fragment.split(openingScriptRe);
-            parsed[0] = parsed[0].replace(affectedTagRe, disableAttributes);
-            if (parsed[1]) parsed[1] = parsed[1].replace(affectedTagRe, disableAttributes);
-            return parsed;
-        });
-
-    return [].concat.apply([], ret).join('');
+Mobify.html.disable = function(htmlStr) {    
+    return htmlStr.replace(affectedTagRe, disableAttributes);
 };
 
 // Returns a string with all disabled external attributes enabled.
