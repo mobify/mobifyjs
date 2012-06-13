afc8bfddb5f2d9e9d3f62aca31bbd2d205d2d153
Noah Adams <noah.adams@gmail.com>
2012-06-13 15:14:53 -0700

    add pure javascript url absolutification to util.

--- api/util.js
+++ api/util.js
@@ -1,5 +1,8 @@
 (function($, Mobify) {
 
+// Initialize the Mobify.util namespace
+Mobify.util || (Mobify.util = {});
+
 // Set optout cookie and reload to goto desktop.
 // V6.X: mobify-path=
 //
@@ -27,5 +30,90 @@ Mobify.i18n = function(list, data) {
     }
     return i18nlookup;
 };
+
+// Pure Javascript URL absolutification courtesy of https://gist.github.com/1254025
+
+var parseURIMatcher = /^([^:\/?#]+:)?(\/\/(?:[^:@]*(?::[^:@]*)?@)?(([^:\/?#]*)(?::(\d*))?))?([^?#]*)(\?[^#]*)?(#[\s\S]*)?/;
+
+/**
+ * Parses the URI
+ *
+ * @param url
+ * @returns object
+ */
+function parseURI(url) {
+    var m = parseURIMatcher.exec(url);
+    // authority = '//' + user + ':' + pass '@' + hostname + ':' port
+    return (m ? {
+        href     : m[0] || '',
+        protocol : m[1] || '',
+        authority: m[2] || '',
+        host     : m[3] || '',
+        hostname : m[4] || '',
+        port     : m[5] || '',
+        pathname : m[6] || '',
+        search   : m[7] || '',
+        hash     : m[8] || ''
+    } : null);
+}
+
+/**
+ * Absolutizes the path in `href` based on `base` url.
+ *
+ * @param base Base URI
+ * @param href Path which to convert absolute
+ *
+ * @returns Absolute URI
+ **/
+function absolutizeURI(base, href) {// RFC 3986
+    function removeDotSegments(input) {
+        var output = [];
+            input.replace(/^(\.\.?(\/|$))+/, '')
+            .replace(/\/(\.(\/|$))+/g, '/')
+            .replace(/\/\.\.$/, '/../')
+            .replace(/\/?[^\/]*/g, function (p) {
+                if (p === '/..') {
+                    output.pop();
+                } else {
+                    output.push(p);
+                }
+            });
+        return output.join('');
+    }
+
+    href = parseURI(String(href || '').replace(/^\s+|\s+$/g, ''));
+    base = parseURI(String(base || '').replace(/^\s+|\s+$/g, ''));
+    if (href === null || base === null) {
+        return null;
+    }
+    var res = {};
+    if (href.protocol || href.authority) {
+        res.authority = href.authority;
+        res.pathname  = removeDotSegments(href.pathname);
+        res.search    = href.search;
+    } else {
+        if (!href.pathname) {
+            res.pathname = base.pathname;
+            res.search   = href.search || base.search;
+        } else {
+            if (href.pathname.charAt(0) === '/') {
+                res.pathname = removeDotSegments(href.pathname);
+            } else {
+                if (base.authority && !base.pathname) {
+                    res.pathname = removeDotSegments('/' + href.pathname);
+                } else {
+                    res.pathname = removeDotSegments(base.pathname.slice(0, base.pathname.lastIndexOf('/') + 1) + href.pathname);
+                }
+            }
+            res.search = href.search;
+        }
+        res.authority = base.authority;
+    }
+    res.protocol = href.protocol || base.protocol;
+    res.hash     = href.hash;
+    return res.protocol + res.authority + res.pathname + res.search + res.hash;
+}
+
+Mobify.util.absolutizeURI = absolutizeURI;
 
 })(Mobify.$, Mobify);
