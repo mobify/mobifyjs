26603a26fd5c53ad0eeb8ee4c51f8c18c7b3ae77
Shawn Jansepar <shawn@mobify.com>
2012-08-09 14:56:07 -0700

    Message passing and transform overrides in place

--- api/studioJS.js
+++ api/studioJS.js
@@ -11,6 +11,16 @@
 
 // if you are in an iframe... and its studio.. then load.
 
+var Modes = {
+        Source: "source"
+      , Preview: "preview"
+    }
+  , mode = Modes.Preview
+
+if (/mobify-studiojs=source/.test(window.location.hash)){
+    mode = Modes.Source;
+}
+
 var $ = Mobify.$
 
   , _tagIndex = 0
@@ -52,48 +62,57 @@ var $ = Mobify.$
      * Send a message to the app.
      */
   , set = function(key, value) {
-        parent.postMessage({key: key, value: value}, '*');
-        // window.postMessage({
-        //     dest: 'extension'
-        //   , command: 'html'
-        //   , key: key
-        //   , value: value
-        // }, '*');
+        parent.postMessage({action: key, value: value}, '*');
     };
 
-/**
- * Rather than just writing the emitted markup to the document... do something
- * else with it! Mwahahah!
- */
-var oldEmitMarkup = Mobify.emitMarkup;
-Mobify.emitMarkup = function(markup) {
-    console.log('new emitMarkup');
-    return oldEmitMarkup(markup);
 
-    // debugger;
-    // Mobify.studioJS.get('renderHTML', function(markup) {
-    //     oldEmitMarkup(markup);
-    // });
-    // Mobify.studioJS.set('resultHTML', markup);
-};
+if (mode == Modes.Source) {
+    /**
+     * In `Source` mode, we render the `source` markup with our special choosey 
+     * JavaScript and send the `result` to the App.
+     */
+    var oldEmitMarkup = Mobify.transform.emitMarkup;
+    Mobify.transform.emitMarkup = function(markup) {    
+      var original = Mobify.html.extractHTML().all();
+      var index = original.indexOf("</body>");
+      original = original.substring(0, index) + "<link rel='stylesheet' href='http://cloud-dev.mobify.com:8000/static/choose/assets/css/index.css'><script data-main='http://cloud-dev.mobify.com:8000/static/choose/app/config' src='http://cloud-dev.mobify.com:8000/static/choose/assets/js/libs/require.js'></script>" + original.substring(index)
+      set('result', markup);
+      oldEmitMarkup(original);
+    };
 
-/**
- * Index the tags in the extracted HTML. Message the page's source HTML to the
- * controller.
- */
-var oldExtractHTML = Mobify.html.extractHTML;
-Mobify.html.extractHTML = function() {
-    var captured = oldExtractHTML();
+    /**
+      * Index the tags in the extracted HTML. Message the page's source HTML to the
+      * controller.
+      */
+    // var oldExtractHTML = Mobify.html.extractHTML;
+    // Mobify.html.extractHTML = function() {
+    //   var captured = oldExtractHTML();
+    //   set('sourceHTML', captured.all());
+    //   //$.each(captured, function(key, value) {
+    //   //    captured[key] = indexTags(value);
+    //   //});
+    //   // Mobify.studioJS.set('sourceHTML', captured.all());
+    //   return captured;
+    // };
 
-    // Send markup to main window!
-    set('sourceHTML', captured.all());
+} else {
+    /**
+     * In `Preview` mode, the frame waits for the `result` HTML from the
+     * app. We render it when it comes in. 
+     */
+    Mobify.transform.run = function() {
+        var onMessage = function(event) {
+                // console.log('PreviewFrame: Got Message:', event);
+                var data = event.data;
+                if (data.action !== 'result') return;
+                document.open();
+                document.write(data.value);
+                document.close();
+            }
 
-    //$.each(captured, function(key, value) {
-    //    captured[key] = indexTags(value);
-    //});
-
-    // Mobify.studioJS.set('sourceHTML', captured.all());
-    return captured;
-};
+        window.addEventListener('message', onMessage, false);
+        // console.log('PreviewFrame: Waiting for result from App.')'
+    }
+}
 
 })(this, Mobify);
