c02691d730277dbb2ebfdb73c80a6035221fa36e
John Boxall <john@mobify.me>
2012-08-13 11:51:43 -0700

    Reset RequireJS on reload to allow module loading side effects to occur.

--- api/studioJS.js
+++ api/studioJS.js
@@ -22,56 +22,19 @@ if (/mobify-studiojs=source/.test(window.location.hash)){
 
 var $ = Mobify.$
 
-  , _tagIndex = 0
-
-    /**
-     * Adds a unique `mobifyjsindex` attribute to nodes found in `html`.
-     */
-  , indexTags = function(html) {
-        var tagIndex = /<![\s\S]*?>|[^<]+|<!--[\s\S]*?-->|(<script)([\s\S]*?<\/script>)|(<\w+)((?:[^>'"]*|'[^']*?'|"[^"]*?")*\/?>)/gi
-        
-        return html.replace(tagIndex, function(all, scriptName, scriptTail, name, tail) {
-            name = name || scriptName;
-            tail = tail || scriptTail;
-            if (!name) return all;
-            return name + ' mobifyjsindex="src' + tagIndex++ + '"' + tail;
-        });
-    }
-
-    // Get the page content and pass it along.
-  , get = function(key, callback) {
-
-        // ???
-        var handler = function(event) {
-                if ((event.data.command === 'html')
-                 && (event.data.dest === 'page')
-                 && (event.data.key === key)
-                 && (event.source === window)) {
-                    
-                    window.removeEventListener("message", handler, false);
-
-                    callback(event.data.value, event.data.key);    
-                }
-            };
-
-        window.addEventListener("message", handler, false);
-    }
-
     /**
      * Send a message to Studio.
      */
-  , set = function(key, value) {
+  , postMessage = function(key, value) {
         parent.postMessage({action: key, value: value}, '*');
     };
 
 
+/**
+ * In `Source` mode, we render the `source` markup with our special choosey 
+ * JavaScript and send the `result` to the App.
+ */
 if (mode == Modes.Source) {
-
-
-    /**
-     * In `Source` mode, we render the `source` markup with our special choosey 
-     * JavaScript and send the `result` to the App.
-     */
     var dataToJson = function(data) {
             var json = {};
 
@@ -101,48 +64,32 @@ if (mode == Modes.Source) {
 
     var oldEmitMarkup = Mobify.transform.emitMarkup;
     Mobify.transform.emitMarkup = function(adaptedHtml) {
+        // console.log('SourceViewIframe: All ready to go!');
         var data = Mobify.evaluatedData;
-
         var json = dataToJson(data);
-
-        // console.log('SourceViewIframe: Ready to message - ', json);
-
         var sourceHtml = Mobify.html.extractHTML().all();
         var index = sourceHtml.indexOf("</body>");
-        // These scripts will also be responsible for binding 'after docwrite'
-        // listeners.
+        if (!index) return console.error('Cannot find </body>.')
+
+        // Load the StudioJS interface. Remove `requirejs` so we can continue
+        // to use our side effect dependant module loading anti-pattern :)
         sourceHtml = sourceHtml.substring(0, index) 
                    + "<link rel='stylesheet' href='http://cloud-dev.mobify.com:8000/static/choose/assets/css/index.css'>"
-                   + "<script data-main='http://cloud-dev.mobify.com:8000/static/choose/app/config' src='http://cloud-dev.mobify.com:8000/static/choose/assets/js/libs/require.js'></script>" 
+                   + "<script>requirejs = require = define = undefined</script>"
+                   + "<script data-main='http://cloud-dev.mobify.com:8000/static/choose/app/config' src='http://cloud-dev.mobify.com:8000/static/choose/assets/js/libs/require.js'></script>"
                    + sourceHtml.substring(index);
-        
-        // We could get rid of some things here.
-        set('result', {html: adaptedHtml, data: json});
+
+        postMessage('result', {html: adaptedHtml, data: json});
         
         oldEmitMarkup(sourceHtml);
     };
 
-    /**
-      * Index the tags in the extracted HTML. Message the page's source HTML to the
-      * controller.
-      */
-    // var oldExtractHTML = Mobify.html.extractHTML;
-    // Mobify.html.extractHTML = function() {
-    //   var captured = oldExtractHTML();
-    //   set('sourceHTML', captured.all());
-    //   //$.each(captured, function(key, value) {
-    //   //    captured[key] = indexTags(value);
-    //   //});
-    //   // Mobify.studioJS.set('sourceHTML', captured.all());
-    //   return captured;
-    // };
-
+/**
+ * In `Preview` mode, await `result` message from the App for rendering.
+ * We render it using `document.write` when it comes in and then setup
+ * to recieve more messages.
+ */
 } else {
-    /**
-     * In `Preview` mode, this window awaits `result` HTML from the App.
-     * We render it using `document.write` when it comes in and then setup
-     * to recieve more messages.
-     */
     Mobify.transform.run = function() {
         var onMessage = function onMessage(event) {
                 // console.log('PreviewFrame: Got Message:', event);
@@ -150,7 +97,9 @@ if (mode == Modes.Source) {
                 if (data.action !== 'result') return;
 
                 var html = data.value
-                var index = html.indexOf('</body')
+                var index = html.indexOf('</body>')
+                if (!index) return console.error('Cannot find </body>.')
+
                 // Rebind to receive the next message.
                 html = html.substring(0, index)
                      + "<script>window.addEventListener('message', " + onMessage.toString() + ", '*');<\/script>" 
