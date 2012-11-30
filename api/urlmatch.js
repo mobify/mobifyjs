b740b5e62d5ce0803a3e932236f7ea45a7cd60af
Noah Adams <noah.adams@gmail.com>
2012-11-30 12:53:25 -0800

    cleanup

--- api/urlmatch.js
+++ api/urlmatch.js
@@ -4,12 +4,12 @@
 (function() {
 
 /**
- * Returns an escaped  string that when passed to the regular expression 
+ * Returns an escaped string that when passed to the regular expression
  * contructor will match the literal contents of the string.
  */
 var reEscape = function (str) {
-    return str.replace(/([.?*+^$[\]\\(){}-])/g, "\\$1");
-}; 
+    return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+};
 
 var declareUrlMatch = function(window, Mobify) {
 
@@ -18,107 +18,83 @@ var declareUrlMatch = function(window, Mobify) {
      * used to match a URL's path.
      */
     var getExpressionRegExp = function(expr) {
-        var SLASH = '/'
-          , WILD = '*'
-          , WILD_RE_STR = '[^\\/]+'
-          , SLASH_RE_STR = '\\/+'
-          , FINAL_WILD_RE = '.+'
-          , TRAILING_SLASH_RE = '(\\/*)?'
-          , reStr = '^'
-          , bits = expr.split(SLASH)
-          , firstRun = true;
-
-        if(expr === "/*") {
-            return /^.*$/;
+        if (expr == "/*") {
+            return /.*/;
         }
+
+        // #    Expr    Split               RE
+        // A    /       [""]                /^\/+$/
+        // B    /*/     ["", "*", ""]       /^\/+[^\/]+\/*$/
+        // C    /a/     ["", "a", ""]       /^\/+a\/*$/
+        // D    /a      ["", "a"]           /^\/+a\/*$/
+        // E    /a/*    ["", "a", "*"]      /^\/+a\/+.+$/
+        // F    /a/b    ["", "a", "b"]      /^\/+a\/+b\/*$/
+        // G    /a/b/   ["", "a", "b", ""]  /^\/+a\/+b\/*$/
+        // H    /a/b/*  ["", "a", "b", "*"] /^\/+a\/+b\/+.+$/
+
+        var EMPTY = "";
+        var WILD = "*";
+        // Merge slashes.
+        var SLASH = "\\/+";
+
+        var bits = expr.slice(1).split("/");
+        var reStr = EMPTY;
+        var bit;
 
         while (bits.length) {
             bit = bits.shift();
-
-            if (bits.length === 0) {
-                if(!firstRun && bit != '') reStr += SLASH_RE_STR;
-                firstRun = false;
-
-                bit = (bit == WILD ? FINAL_WILD_RE : reEscape(bit));
-                reStr += bit + TRAILING_SLASH_RE;
+            if (bits.length) {
+                reStr += SLASH + ((bit == WILD) ? "[^\\/]+" : reEscape(bit));
             } else {
-                if (bit === '') {
-                    firstRun = false;
-                    continue;
+                // E
+                if (bit == WILD) {
+                    reStr += ".+"
+                } else {
+                    // D
+                    if (bit != EMPTY) {
+                        reStr += SLASH + reEscape(bit)
+                    }
+                    reStr += '\\/*';
                 }
-                if(!firstRun) reStr += SLASH_RE_STR;
-                firstRun = false;
-
-                bit = (bit == WILD ? WILD_RE_STR : reEscape(bit));
-                reStr += bit;
             }
         }
 
-        var re = new RegExp(reStr + "$", "i");
-        console.log(re);
-        return re;
-
-
-        // var SLASH = '/';
-        // var WILD = '*';
-        // var WILD_RE_STR = '.*';
-        // var SLASH_RE_STR = '\\/*';
-
-        // var bits = expr.slice(1).split(SLASH);
-        // var reStr = SLASH_RE_STR;
-
-        // console.log(bits)
-
-        // while (bits.length) {
-        //     bit = bits.pop();
-        //     if (bit === '') continue;
-
-            // bit = (bit == WILD ? WILD_RE_STR : reEscape(bit));
-            // reStr = SLASH_RE_STR + bit + reStr;
-
-        //     WILD_RE_STR = '[^\/]+';
-        //     SLASH_RE_STR = '\\/+';
-        // };
-
-        // var re = new RegExp("^\\/" + reStr + "$", "i");
-        // console.log(re);
-        // return re;
+        var re = RegExp("^" + reStr + "$", "i");
+        return re
     };
 
     /**
-     * Given a path expression `expr`, or a regular expression, returns a 
-     * function that can be used to match against the current window's path, 
+     * Given a path expression `expr`, or a regular expression, returns a
+     * function that can be used to match against the current window's path,
      * `window.location.pathname`.
      */
     var urlmatch = function(expr) {
         var exprIsRegExp, exprIsString, re;
-        
+
         exprIsRegExp = expr instanceof RegExp;
         exprIsString = (typeof expr === 'string');
-        
+
         if (!(exprIsRegExp || exprIsString)) {
             return false;
         }
         if (exprIsRegExp) {
             re = expr;
-        } else { 
+        } else {
             re = getExpressionRegExp(expr);
         }
         return function() {
-            // Note, window is the closed-overarguemnt to the parent function, 
+            // Note, window is the closed-overarguemnt to the parent function,
             // not necessarily the global window
             return re.test(window.location.pathname) ? expr : false;
         };
     };
 
-    /* 
-    * Set a new object with a location property for the urlmatch function to be 
-    * used in place window.
-    */
+    /**
+     * Allow the local variable `window` to be overridden. Useful for testing.
+     */
     urlmatch.setWindow = function (newWindow) {
-        // change closed over window
         window = newWindow;
-    }
+    };
 
     return urlmatch;
 };
