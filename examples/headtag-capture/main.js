75198032c7f6469a1f5109322b42e3c8548c5ac8
Shawn Jansepar <shawn@mobify.com>
2013-01-15 18:51:16 -0800

    Added better usage examples

--- examples/headtag-capture/main.js
+++ examples/headtag-capture/main.js
@@ -1,16 +1,19 @@
 capturing = window.capturing;
 if (capturing) {
     console.log("Executing main during capturing phase!")
+
+    // Grab Zepto reference to escaped source DOM object
     var $html = Mobify.Capture.getSourceDOM();
 
+    // Uncomment to remove all scripts in document
     //$html.find("script").remove();
-    $html.find("img").resizeImages();
 
-    //Mobify.Lazyload.rewriteSrc($html[0]);
+    // Resize images using Mobify Image Resizer
+    $html.find("img").resizeImages(320);
 
+    // Render source DOM to document
     Mobify.Capture.renderSourceDOM({injectMain: true});
 
 } else {
     console.log("Executing main in post-capturing phase!");
-    //Mobify.Lazyload.attachLazyloadEvents($html, true);
 }
