8a7ac27e54cce4f232d932d981ae535eab095302
Shawn Jansepar <shawn@mobify.com>
2013-01-15 17:25:21 -0800

    Adding enhance.js bootstrapper file

---
+++ src/mobify-enhance.js
@@ -0,0 +1,7 @@
+require(["capture", "enhance"], function(Capture, Enhance) {
+    var Mobify = window.Mobify = window.Mobify || {};
+    Mobify.Capture = Capture;
+    Mobify.Enhance = Enhance;
+    return Mobify
+}, undefined, true);
+// relPath, forceSync
