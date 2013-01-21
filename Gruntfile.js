51f1838e9846e85e896209b9ea35706c3948ae65
Shawn Jansepar <shawn@mobify.com>
2013-01-21 11:27:03 -0800

    Adding ability to run qunit tests in saucelabs cloud.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,19 @@
 // http://stackoverflow.com/questions/13567312/working-project-structure-that-uses-grunt-js-to-combine-javascript-files-using-r
 
+
 /*global module:false*/
 module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: '<json:package.json>',
+        localConfig: (function(){ 
+                        try { 
+                            return grunt.file.readJSON('localConfig.json') 
+                        } catch(e) {
+                            return {} 
+                        }
+                    })() ,
         meta: {
           banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
             '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
@@ -83,12 +91,37 @@ module.exports = function(grunt) {
           files: 'src/**/*.js',
           tasks: ['requirejs'],
         },
+        'saucelabs-qunit': {
+            all: {
+                username: '<%= localConfig.saucelabs.username %>', // if not provided it'll default to ENV SAUCE_USERNAME (if applicable)
+                key: '<%= localConfig.saucelabs.key %>', // if not provided it'll default to ENV SAUCE_ACCESS_KEY (if applicable)
+                urls: ['http://localhost:3000/tests/capture.html'],
+                concurrency: 2,
+                tunneled: true,
+                browsers: [{
+                    browserName: 'chrome'
+                }], // https://saucelabs.com/docs/browsers
+                onTestComplete: function(){
+                    // Called after a qunit unit is done, per page, per browser
+                    // Return true or false, passes or fails the test
+                    // Returning undefined does not alter the test result
+
+                    // For async return, call 
+                    var done = this.async();
+                    setTimeout(function(){
+                        // Return to this test after 1000 milliseconds
+                        done(/*true or false changes the test result, undefined does not alter the result*/);
+                    }, 1000);
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-requirejs');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-saucelabs');
 
     // Default task.
     // grunt.registerTask('default', 'lint qunit requirejs');
@@ -100,5 +133,6 @@ module.exports = function(grunt) {
     grunt.registerTask('capture', 'requirejs:capture');
     grunt.registerTask('full', 'requirejs:full');
     grunt.registerTask('test', ['connect', 'qunit']);
+    grunt.registerTask('saucelabs', ['test', 'saucelabs-qunit']);
     grunt.registerTask('serve', 'connect');
 };
