c035ad66f4c74de5ba97e6ce2f1cf046ce085262
Shawn Jansepar <shawn@mobify.com>
2013-02-19 15:50:59 -0800

    Hooowww did all these old files creep back?! Removing!

--- tests/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-node tests/server.js &
-PID=$!
-sleep 1
-phantomjs tests/phantom.js | grep '<*>' | tee report.xml
-kill $PID
--- tests/server.js
+++
@@ -1,6 +0,0 @@
-var Connect = require('connect')
-  , server = new Connect().use(Connect.static(process.cwd()))
-  , PORT = 1337
-
-server.listen(PORT);
-console.log('Mobify.js Testing Server @ http://127.0.0.1:' + PORT);
