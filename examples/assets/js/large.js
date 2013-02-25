b0e17b71a72e68a51c2ebea48c8bbb784f2e1f69
Shawn Jansepar <shawn@mobify.com>
2013-02-25 15:53:05 -0800

    Updating media query

---
+++ examples/assets/js/large.js
@@ -0,0 +1,1 @@
+alert("I'm executed from a script loaded from a media query that only loads if the browser with is at least 480px wide! Refresh again with a smaller width to see the other script load");
---
+++ examples/assets/js/small.js
@@ -0,0 +1,1 @@
+alert("I'm executed from a script loaded from a media query that only loads if the browser with is at most 480px wide! Refresh again with a larger width to see the other script load.");
