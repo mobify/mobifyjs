95c19240c5f223d6bf0adb7b8a9c9ccc8222b546
Shawn Jansepar <shawn@mobify.com>
2013-02-25 16:23:48 -0800

    Updating text, removing old example

--- examples/assets/js/large.js
+++ examples/assets/js/large.js
@@ -1,1 +1,5 @@
-alert("I'm executed from a script loaded from a media query that only loads if the browser with is at least 480px wide! Refresh again with a smaller width to see the other script load");
+window.onload = function(){
+
+    alert("I'm executed from a script loaded from a media query that only loads if the browser with is at least 480px wide! Refresh again with a smaller width to see the other script load");
+
+}
--- examples/assets/js/small.js
+++ examples/assets/js/small.js
@@ -1,1 +1,5 @@
-alert("I'm executed from a script loaded from a media query that only loads if the browser with is at most 480px wide! Refresh again with a larger width to see the other script load.");
+window.onload = function(){
+
+    alert("I'm executed from a script loaded from a media query that only loads if the browser with is at most 480px wide! Refresh again with a larger width to see the other script load.");
+
+}
