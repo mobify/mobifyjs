43b92a0e714a8928173c77655bd3ee86efea23db
Noah Adams <noah.adams@gmail.com>
2013-02-28 16:15:28 -0800

    looted localconfig and s3 config things from mobifyjs v2

--- www/static/modules/accordion/Gruntfile.js
+++ www/static/modules/accordion/Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        localConfig: (function(){ 
+                        try { 
+                            return grunt.file.readJSON('localConfig.json') 
+                        } catch(e) {
+                            return {};
+                        }
+                    })(),
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,6 +18,25 @@ module.exports = function(grunt) {
                 src: 'accordion.js',
                 dest: 'build/<%= pkg.name %>.<%= pkg.version %>.min.js'
             }
+        },
+        s3: {
+            key: '<%= localConfig.aws.key %>',
+            secret: '<%= localConfig.aws.secret %>',
+            bucket: '<%= localConfig.aws.bucket %>',
+            access: "public-read",
+            headers: { "Cache-Control": "max-age=1200" },
+            upload: [
+                { // build
+                    src: "build/**/*",
+                    dest: "mobifyjs/build/",
+                    rel: "build"
+                },
+                { // examples
+                    src: "examples/**/*",
+                    dest: "mobifyjs/examples/",
+                    rel: "examples"
+                }
+            ]
         }
     });
 
