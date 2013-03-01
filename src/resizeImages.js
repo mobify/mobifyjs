e81e3d0613429483b080b65b8d5187597522f56a
Shawn Jansepar <shawn@mobify.com>
2013-03-01 14:36:03 -0800

    Adding Roman's code for auto-detecting screen width/height into ResizeImage library

--- src/resizeImages.js
+++ src/resizeImages.js
@@ -9,7 +9,48 @@ var httpRe = /^https?/
 
 // A protocol relative URL for the host ir0.mobify.com.
 var PROTOCOL_AND_HOST = '//ir0.mobify.com'
-          
+     
+function getPhysicalScreenSize() {
+    
+    function multiplyByPixelRatio(sizes) {
+        var dpr = window.devicePixelRatio || 1;
+
+        sizes.width = Math.round(sizes.width * dpr);
+        sizes.height = Math.round(sizes.height * dpr);
+        
+        return sizes;
+    }
+
+    var iOS = navigator.userAgent.match(/iphone|ipod|ipad/i);
+    var androidVersion = (navigator.userAgent.match(/android (\d)/i) || {})[1];
+
+    var sizes = {
+        width: window.outerWidth
+      , height: window.outerHeight
+    };
+
+    // Old Android and BB10 use physical pixels in outerWidth/Height, which is what we need
+    // New Android (4.0 and above) use CSS pixels, requiring devicePixelRatio multiplication
+    // iOS lies about outerWidth/Height when zooming, but does expose CSS pixels in screen.width/height
+
+    if (!iOS) {
+        if (androidVersion > 3) return multiplyByPixelRatio(sizes);
+        return sizes;
+    }
+
+    var isLandscape = window.orientation % 180;
+
+    if (isLandscape) {
+        sizes.height = screen.width;
+        sizes.width = screen.height;
+    } else {
+        sizes.width = screen.width;
+        sizes.height = screen.height;
+    }
+
+    return multiplyByPixelRatio(sizes);
+};
+
 /**
  * Returns a URL suitable for use with the 'ir' service.
  */ 
@@ -53,14 +94,12 @@ ResizeImages.resize = function(imgs, options) {
     }
     var dpr = window.devicePixelRatio;
 
+    var screenSize = getPhysicalScreenSize();
+    var width = opts.maxWidth || screenSize.width;
+    var height = opts.maxHeight || screenSize.height;
     if (dpr) {
-        if (opts.maxWidth) {
-            opts.maxWidth = Math.ceil(opts.maxWidth * dpr);
-        }
-
-        if (opts.maxHeight) {
-            opts.maxHeight = Math.ceil(opts.maxHeight * dpr);
-        }
+        opts.maxWidth = Math.ceil(width * dpr);
+        opts.maxHeight = Math.ceil(height * dpr);
     }
 
     var attr;
