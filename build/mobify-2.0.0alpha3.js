76c49521bf08a461690fb0fee8a799ba65323e32
Shawn Jansepar <shawn@mobify.com>
2013-05-31 12:18:20 -0700

    Adding synchronous regex based WEBP test.

--- build/mobify-2.0.0alpha3.js
+++ build/mobify-2.0.0alpha3.js
@@ -970,6 +970,69 @@ var httpRe = /^https?/;
 // A protocol relative URL for the host ir0.mobify.com
 var PROTOCOL_AND_HOST = '//6.wesleycrush.appspot.com';
 
+function persistWebpSupport(supported) {
+    document.cookie = "webpSupport=" + supported + "; path=/"
+}
+
+// Synchronous WEBP detection using regex
+// Avoiding async way of detecting due to performance reasons
+// (onload of detector image won't fire until document is complete)
+function webpDetector(userAgent){
+    var userAgent = userAgent || navigator.userAgent;
+
+    // Check if WEBP support has already been detected
+    var match = /webpSupport=([^&;]*)/.exec(document.cookie);
+    if (match && match[1] == "0") {
+        return 0;
+    } else if (match && match[1] == "1") {
+        return 1;
+    }
+
+    function regexDetect(userAgent){
+        // Credit to Ilya Grigorik for WEBP regex matching
+        // https://github.com/igrigorik/webp-detect/blob/master/pagespeed.cc
+        var supportedRe = /(Android\s|Chrome\/|Opera9.8*Version\/..\.|Opera..\.)/i;
+        var unsupportedVersionsRe = new RegExp('(Android\\s(0|1|2|3)\\.)|(Chrome\\/[0-8]\\.)' +
+                                    '|(Chrome\\/9\\.0\\.)|(Chrome\\/1[4-6]\\.)|(Android\\sChrome\\/1.\\.)' +
+                                    '|(Android\\sChrome\\/20\\.)|(Opera\\/9\\.80*Version\\/10\\.)' + 
+                                    '|(Opera\\/9\\.80*Version\\/11\\.0)|(Opera.10\\.)|(Opera.11\\.)' +
+                                    '|(Chrome\\/(1.|20|21|22)\\.)', 'i');
+
+        // Return false if browser is not supported
+        if (!supportedRe.test(userAgent)) {
+            return 0;
+        }
+
+        // Return false if browser is supported, but the version
+        // is known to not support WEBP.
+        if (unsupportedVersionsRe.test(userAgent)) {
+            return 0;  
+        }
+        return 1;
+    }
+
+    function dataUriDetect() {
+        // Credit to Modernizer for data uri detection method
+        // https://github.com/Modernizr/Modernizr/blob/fb76d75fbf97f715e666b55b8aa04e43ef809f5e/feature-detects/img-webp.js
+        var image = new Image();
+        image.onload = function() {
+            persistWebpSupport((image.width == 4) ? 1 : 0);
+        };
+        image.src = 'data:image/webp;base64,UklGRjgAAABXRUJQVlA4ICwAAAAQAgCdASoEAAQAAAcIhYWIhYSIgIIADA1gAAUAAAEAAAEAAP7%2F2fIAAAAA';
+    }
+
+    // Run async WEBP detection for future proofing
+    // This test may not finish running before the first call of `resize`
+    dataUriDetect();
+
+    // Run regex based synchronous WEBP detection
+    var support = regexDetect(userAgent);
+    persistWebpSupport(support);
+
+    return support;
+
+}
+
 function getPhysicalScreenSize(devicePixelRatio) {
 
     function multiplyByPixelRatio(sizes) {
@@ -1100,19 +1163,8 @@ ResizeImages.resize = function(imgs, options) {
 var defaults = {
       projectName: "oss-" + location.hostname.replace(/[^\w]/g, '-'),
       attribute: "x-src",
-      webp: false
+      webp: webpDetector()
 };
-
-// webp feature detect
-// https://github.com/Modernizr/Modernizr/blob/fb76d75fbf97f715e666b55b8aa04e43ef809f5e/feature-detects/img-webp.js
-(function(){
-    var image = new Image();
-    image.onload = function() {
-        defaults.webp = (image.width == 4);
-    };
-    image.src = 'data:image/webp;base64,UklGRjgAAABXRUJQVlA4ICwAAAAQAgCdASoEAAQAAAcIhYWIhYSIgIIADA1gAAUAAAEAAAEAAP7%2F2fIAAAAA';
-
-})();
 
 return ResizeImages;
 
