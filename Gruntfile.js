390b14e8c86ce4c541167e088d529e4e650d24bc
Noah Adams <noah.adams@gmail.com>
2013-06-25 10:09:30 -0700

    moves bucket config into grunt file, adds some constants for long and short caching cache control headers

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 // http://stackoverflow.com/questions/13567312/working-project-structure-that-uses-grunt-js-to-combine-javascript-files-using-r
 var fs = require("fs");
 
+var LONG_CACHE_CONTROL = "public,max-age=31536000"; // one year
+var SHORT_CACHE_CONTROL = "public,max-age=300;" // five minutes
+
 /*global module:false*/
 module.exports = function(grunt) {
 
@@ -225,12 +228,13 @@ module.exports = function(grunt) {
                 key: '<%= localConfig.aws.key %>',
                 secret: '<%= localConfig.aws.secret %>',
                 access: "public-read",
-                headers: { "Cache-Control": "max-age=1200" },
-                gzip: true
+                headers: { "Cache-Control": SHORT_CACHE_CONTROL },
+                maxOperations: 6
             },
             build: {
                 options: {
-                    bucket: '<%= localConfig.aws.buckets.cdn %>'
+                    bucket: '<%= localConfig.aws.buckets.cdn %>',
+                    gzip: true
                 },
                 upload: [
                     { // build
@@ -242,7 +246,8 @@ module.exports = function(grunt) {
             },
             examples: {
                 options: {
-                    bucket: '<%= localConfig.aws.buckets.cdn %>'
+                    bucket: 'mobify',
+                    gzip: true
                 },
                 upload: [
                     { // examples
@@ -254,26 +259,38 @@ module.exports = function(grunt) {
             },
             wwwstaging: {
                 options: {
-                    bucket: '<%= localConfig.aws.buckets.wwwstaging %>',
+                    bucket: 'www-staging.mobify.com'
                 },
                 upload: [
-                    { 
+                    {
+                        src: "www/_site/static/**/*",
+                        dest: "mobifyjs/static",
+                        rel: "www/_site",
+                        headers: { "Cache-Control": LONG_CACHE_CONTROL }
+                    },
+                    {
                         src: "www/_site/**/*",
                         dest: "mobifyjs",
-                        rel: "www/_site",
-                    },
+                        rel: "www/_site"
+                    }
                 ]
             },
             www: {
                 options: {
-                    bucket: '<%= localConfig.aws.buckets.wwwprod %>',
+                    bucket: 'www.mobify.com'
                 },
                 upload: [
-                    { 
-                        src: "www/_site/**/*",
-                        dest: "mobifyjs",
+                    {
+                        src: "www/_site/static/**/*",
+                        dest: "mobifyjs/static",
                         rel: "www/_site",
-                    },
+                        headers: { "Cache-Control": LONG_CACHE_CONTROL }
+                    }, 
+                    // {
+                    //     src: "www/_site/**/*",
+                    //     dest: "mobifyjs",
+                    //     rel: "www/_site"
+                    // }
                 ]
             }
         },
