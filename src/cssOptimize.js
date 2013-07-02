216008b8ef1884e508398a2d783a505b40079145
Noah Adams <noah.adams@gmail.com>
2013-07-02 14:19:18 -0700

    cssOptimizer Client improvements
    - add error handler for <link> element loading
    - rename some defaults used for attribute swapping

--- src/cssOptimize.js
+++ src/cssOptimize.js
@@ -32,12 +32,15 @@ CssOptimize.getCssURL = function(url, options) {
  * our service.
  */
 CssOptimize._rewriteHref = function(element, options) {
-    var attributeVal = element.getAttribute(options.attribute);
+    var attributeVal = element.getAttribute(options.targetAttribute);
     var url;
     if(attributeVal) {
+        element.setAttribute('data-orig-href', url);
+        element.setAttribute('onerror', options.onerror);
         url = Utils.absolutify(attributeVal);
         if (Utils.httpUrl(url)) {
-            element.setAttribute(options.attribute, CssOptimize.getCssURL(url));
+            element.setAttribute(options.targetAttribute,
+                                 CssOptimize.getCssURL(url));
         }
     }
 };
@@ -52,10 +55,24 @@ CssOptimize.optimize = function(elements, options) {
     for(var i = 0, len = elements.length; i < len; i++) {
         element = elements[i];
         if (element.nodeName === 'LINK' &&
-          element.getAttribute('rel') === 'stylesheet' &&
-          element.getAttribute(opts.attribute)) {
+            element.getAttribute('rel') === 'stylesheet' &&
+            element.getAttribute(opts.targetAttribute)) {
+
             CssOptimize._rewriteHref(element, opts);
         }
+    }
+};
+
+/**
+ * An 'error' event handler designed to be set using an "onerror" attribute that
+ * will set the target elements "href" attribute to the value of its 
+ * "data-orig-href" attribute, if one exists.
+ */
+var restoreOriginalHref = CssOptimize.restoreOriginalHref = function(event) {
+    var origHref;
+    event.target.removeAttribute('onerror'); //remove error handler
+    if(origSrc = event.target.getAttribute('data-orig-href')) {
+        event.target.setAttribute('href', origHref);
     }
 };
 
@@ -64,9 +81,9 @@ var defaults = {
     host: 'jazzcat.mobify.com',
     endpoint: 'cssoptimizer',
     projectName: 'oss-' + location.hostname.replace(/[^\w]/g, '-'),
-    attribute: 'x-href'
+    targetAttribute: 'x-href',
+    onerror: 'CssOptimize.restoreOriginalHref(event);'
 };
 
-
-return CssOptimize
+return CssOptimize;
 });
