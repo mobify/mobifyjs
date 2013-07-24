e18956f44f669c4c7ad5c1b7457aff22a45202d0
Shawn Jansepar <shawn@mobify.com>
2013-07-24 13:39:13 -0700

    Fixing CssOptimize typo

--- build/mobify-2.0.0alpha4.js
+++ build/mobify-2.0.0alpha4.js
@@ -2161,7 +2161,7 @@ return;
 
 });
 
-require(["mobifyjs/utils", "mobifyjs/capture", "mobifyjs/resizeImages", "mobifyjs/jazzcat", "mobifyjs/unblockify", "mobifyjs/cssOptimize", "mobifyjs/external/picturefill"], function(Utils, Capture, ResizeImages, Jazzcat, Unblockify, cssOptimize) {
+require(["mobifyjs/utils", "mobifyjs/capture", "mobifyjs/resizeImages", "mobifyjs/jazzcat", "mobifyjs/unblockify", "mobifyjs/cssOptimize", "mobifyjs/external/picturefill"], function(Utils, Capture, ResizeImages, Jazzcat, Unblockify, CssOptimize) {
     var Mobify = window.Mobify = window.Mobify || {};
     Mobify.Utils = Utils;
     Mobify.Capture = Capture;
