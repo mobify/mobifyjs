ed10f8e6533aabee3c8aaef4afd2f92c89463bfd
Shawn Jansepar <shawn@mobify.com>
2013-08-02 15:23:50 -0700

    Bumping version number

--- build/mobify-2.0.0alpha6.js
+++ build/mobify-2.0.0alpha6.js
@@ -550,6 +550,25 @@ Utils.matchMedia = function(doc) {
     };
 };
 
+// readyState: loading --> interactive --> complete
+//                      |               |
+//                      |               |
+//                      v               v
+// Event:        DOMContentLoaded    onload
+//
+// iOS 4.3 and some Android 2.X.X have a non-typical "loaded" readyState,
+// which is an acceptable readyState to start capturing on, because
+// the data is fully loaded from the server at that state.
+// For some IE (IE10 on Lumia 920 for example), interactive is not 
+// indicative of the DOM being ready, therefore "complete" is the only acceptable
+// readyState for IE10
+// Credit to https://github.com/jquery/jquery/commit/0f553ed0ca0c50c5f66377e9f2c6314f822e8f25
+// for the IE10 fix
+Utils.domIsReady = function(doc) {
+    var doc = doc || document;
+    return doc.attachEvent ? doc.readyState === "complete" : doc.readyState !== "loading";
+}
+
 return Utils;
 
 });
@@ -666,21 +685,8 @@ var init = Capture.init = function(callback, doc, prefix) {
         var capture = new Capture(doc, prefix);
         callback(capture);
     }
-    // readyState: loading --> interactive --> complete
-    //                      |               |
-    //                      |               |
-    //                      v               v
-    // Event:        DOMContentLoaded    onload
-    //
-    // iOS 4.3 and some Android 2.X.X have a non-typical "loaded" readyState,
-    // which is an acceptable readyState to start capturing on, because
-    // the data is fully loaded from the server at that state.
-    // For some IE (IE10 on Lumia 920 for example), interactive is not 
-    // indicative of the DOM being ready, therefore "complete" is the only acceptable
-    // readyState for IE10
-    // Credit to https://github.com/jquery/jquery/commit/0f553ed0ca0c50c5f66377e9f2c6314f822e8f25
-    // for the IE10 fix
-    if (document.attachEvent ? doc.readyState === "complete" : doc.readyState !== "loading") {
+
+    if (Utils.domIsReady(doc)) {
         createCapture(callback, doc, prefix);
     }
     // We may be in "loading" state by the time we get here, meaning we are
@@ -1232,6 +1238,12 @@ ResizeImages._rewriteSrcAttribute = function(element, opts, srcVal){
         if (Utils.httpUrl(url)) {
             element.setAttribute(opts.targetAttribute, ResizeImages.getImageURL(url, opts));
             element.setAttribute('data-orig-src', srcVal);
+            // if using resize when not capturing, remove the sourceAttribute
+            // as long as it's not "src", which is the target attribute used
+            // when not capturing.
+            if (!capturing && opts.sourceAttribute != opts.targetAttribute) {
+                element.removeAttribute(opts.sourceAttribute);
+            }
             if(opts.onerror) {
                 element.setAttribute('onerror', opts.onerror);
             }
@@ -1518,7 +1530,7 @@ define('jazzcat',["utils", "capture"], function(Utils, Capture) {
                 canSave = true;
                 callback && callback();
             };
-            if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+            if (Utils.domIsReady()) {
                 store();
             }
             else {
