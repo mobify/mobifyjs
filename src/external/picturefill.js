612f162ab9fc810fdbc542c008cab381bedc4148
Shawn Jansepar <shawn@mobify.com>
2013-12-01 20:35:58 -0800

    Update code to use both `picture` and `span[data-picture]`

--- src/external/picturefill.js
+++ src/external/picturefill.js
@@ -8,7 +8,7 @@ if (capturing) {
     Capture.prototype.renderCapturedDoc = function(options) {
         // Change attribute of any img element inside a picture element
         // so it does not load post-flood
-        var imgsInPicture = this.capturedDoc.querySelectorAll('picture img');
+        var imgsInPicture = this.capturedDoc.querySelectorAll('picture img, span[data-picture] img');
         for (var i = 0, len = imgsInPicture.length; i < len; i++) {
             var disableImg = imgsInPicture[i];
             var srcAttr = window.Mobify && window.Mobify.prefix + 'src';
@@ -23,23 +23,12 @@ if (capturing) {
 
 window.matchMedia = window.matchMedia || Utils.matchMedia(document);
 
-/* https://github.com/Wilto/picturefill-proposal */
-/*! Picturefill - Author: Scott Jehl, 2012 | License: MIT/GPLv2 */ 
-/*
-    Picturefill: A polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
-    * Notes: 
-        * For active discussion of the picture element, see http://www.w3.org/community/respimg/
-        * While this code does work, it is intended to be used only for example purposes until either:
-            A) A W3C Candidate Recommendation for <picture> is released
-            B) A major browser implements <picture>
-*/ 
+/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
+
 (function( w ){
+
     // Enable strict mode
     "use strict";
-
-    // User preference for HD content when available
-    var prefHD = false || w.localStorage && w.localStorage[ "picturefill-prefHD" ] === "true",
-        hasHD;
 
     // Test if `<picture>` is supported natively, if so, exit - no polyfill needed.
     if ( !!( w.document.createElement( "picture" ) && w.document.createElement( "source" ) && w.HTMLPictureElement ) ){
@@ -47,14 +36,11 @@ window.matchMedia = window.matchMedia || Utils.matchMedia(document);
     }
 
     w.picturefill = function() {
-        var ps = w.document.getElementsByTagName( "picture" );
-
+        var ps = w.document.querySelectorAll( "picture, span[data-picture]" );
         // Loop the pictures
         for( var i = 0, il = ps.length; i < il; i++ ){
-            var sources = ps[ i ].getElementsByTagName( "source" ),
-                picImg = null,
+            var sources = ps[ i ].querySelectorAll( "span, source" ),
                 matches = [];
-
             // If no sources are found, they're likely erased from the DOM. Try finding them inside comments.
             if( !sources.length ){
                 var picText =  ps[ i ].innerHTML,
@@ -66,9 +52,9 @@ window.matchMedia = window.matchMedia || Utils.matchMedia(document);
                 sources = frag.getElementsByTagName( "div" );
             }
 
-            // See which sources match
+            // See if which sources match
             for( var j = 0, jl = sources.length; j < jl; j++ ){
-                var media = sources[ j ].getAttribute( "media" );
+                var media = sources[ j ].getAttribute( "data-media" ) || sources[ j ].getAttribute( "media" );
                 // if there's no media specified, OR w.matchMedia is supported 
                 if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
                     matches.push( sources[ j ] );
@@ -76,48 +62,26 @@ window.matchMedia = window.matchMedia || Utils.matchMedia(document);
             }
 
             // Find any existing img element in the picture element
-            picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+            var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
 
             if( matches.length ){
-                // Grab the most appropriate (last) match.
-                var match = matches.pop(),
-                    srcset = match.getAttribute( "srcset" );
-
-                if( !picImg ){
+                var matchedEl = matches.pop();
+                if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
                     picImg = w.document.createElement( "img" );
-                    picImg.alt = ps[ i ].getAttribute( "alt" );
-                    ps[ i ].appendChild( picImg );
+                    picImg.alt = ps[ i ].getAttribute( "data-alt" );
+                }
+                else if( matchedEl === picImg.parentNode ){
+                    // Skip further actions if the correct image is already in place
+                    continue;
                 }
 
-                if( srcset ) {
-                        var screenRes = ( prefHD && w.devicePixelRatio ) || 1, // Is it worth looping through reasonable matchMedia values here?
-                            sources = srcset.split(","); // Split comma-separated `srcset` sources into an array.
-
-                        hasHD = w.devicePixelRatio > 1;
-
-                        for( var res = sources.length, r = res - 1; r >= 0; r-- ) { // Loop through each source/resolution in `srcset`.
-                            var source = sources[ r ].replace(/^\s*/, '').replace(/\s*$/, '').split(" "), // Remove any leading whitespace, then split on spaces.
-                                resMatch = parseFloat( source[1], 10 ); // Parse out the resolution for each source in `srcset`.
-
-                            if( screenRes >= resMatch ) {
-                                if( picImg.getAttribute( "src" ) !== source[0] ) {
-                                    var newImg = document.createElement("img");
-
-                                    newImg.src = source[0];
-                                    // When the image is loaded, set a width equal to that of the original’s intrinsic width divided by the screen resolution:
-                                    newImg.onload = function() {
-                                        // Clone the original image into memory so the width is unaffected by page styles:
-                                        this.width = ( this.cloneNode( true ).width / resMatch );
-                                    }
-                                    picImg.parentNode.replaceChild( newImg, picImg );
-                                }
-                                break; // We’ve matched, so bail out of the loop here.
-                            }
-                        }
-                } else {
-                    // No `srcset` in play, so just use the `src` value:
-                    picImg.src = match.getAttribute( "src" );
-                }
+                picImg.src = matchedEl.getAttribute( "data-src" ) || matchedEl.getAttribute("src");
+                matchedEl.appendChild( picImg );
+                picImg.removeAttribute("width");
+                picImg.removeAttribute("height");
+            }
+            else if( picImg ){
+                picImg.parentNode.removeChild( picImg );
             }
         }
     };
@@ -135,7 +99,8 @@ window.matchMedia = window.matchMedia || Utils.matchMedia(document);
     else if( w.attachEvent ){
         w.attachEvent( "onload", w.picturefill );
     }
-})( this );
+
+}( this ));
 
 return;
 
