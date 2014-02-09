36556d66ef12fa9a87aad30073f55ac7c7a02109
Shawn Jansepar <shawn@mobify.com>
2014-02-09 12:14:12 -0800

    Remove "Capture" library as a dependancy of Jazzcat

--- src/jazzcat.js
+++ src/jazzcat.js
@@ -21,7 +21,7 @@
  * into the cache using a bootloader request to Jazzcat. Scripts are then
  * executed directly from the cache.
  */
-define(["mobifyjs/utils", "mobifyjs/capture"], function(Utils, Capture) {
+define(["mobifyjs/utils"], function(Utils) {
     /**
      * An HTTP 1.1 compliant localStorage backed cache.
      */
