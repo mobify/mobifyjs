8bfeb35ea9d4e330e3ac5c92787f294a5ccef410
Ryan J Daw <ryan@mobify.me>
2014-02-10 11:01:30 -0800

    Splits out supportedBrowser from the rest of the logic

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,7 @@ module.exports = function(grunt) {
                   'http://localhost:3000/tests/unblockify.html',
                   'http://localhost:3000/tests/cssOptimize.html',
                   'http://localhost:3000/tests/anchor-test.html',
+                  'http://localhost:3000/tests/supported-browser.html',
                   'http://localhost:3000/tests/tag.html',
                   'http://localhost:3000/tests/tag-integration-tests.html',
                   'http://localhost:3000/tests/tag-old-browser.html'
@@ -106,6 +107,7 @@ module.exports = function(grunt) {
                         'http://localhost:3000/tests/unblockify.html',
                         'http://localhost:3000/tests/cssOptimize.html',
                         'http://localhost:3000/tests/anchor-test.html',
+                        'http://localhost:3000/tests/supported-browser.html',
                         'http://localhost:3000/tests/tag.html',
                         'http://localhost:3000/tests/tag-integration-tests.html'
                     ],
@@ -211,6 +213,7 @@ module.exports = function(grunt) {
                     key: '<%= localConfig.saucelabs.key %>', // if not provided it'll default to ENV SAUCE_ACCESS_KEY (if applicable)
                     urls: [
                         'http://localhost:3000/tests/tag-old-browser.html',
+                        'http://localhost:3000/tests/supported-browser.html'
                     ],
                     concurrency: 16,
                     tunneled: true,
--- tag/v7.js
+++ tag/v7.js
@@ -253,40 +253,6 @@ var collectTiming = function() {
 };
 Private['collectTiming'] = collectTiming;
 
-/** 
-    supportedBrowser will return whether or not we are on a device
-    
-    @private
-    @param {string} ua User agent to test
-    @type {bool}
-*/
-var supportedBrowser = function(ua) {
-    // We're enabled for:
-    // - WebKit based browsers
-    // - IE 10+
-    // - FireFox 4+
-    // - Opera 11+
-    var match = /webkit|(firefox)[\/\s](\d+)|(opera)[\s\S]*version[\/\s](\d+)|(trident)[\/\s](\d+)/i.exec(ua);
-    if (!match) {
-        return false;
-    }
-    // match[1] == Firefox
-    if (match[1] && +match[2] < 4) {
-        return false;
-    }
-    // match[3] == Opera
-    if (match[3] && +match[4] < 11) {
-        return false;
-    }
-    // match[5] == IE
-    if (match[5] && +match[6] < 6) {
-        return false;
-    }
-
-    return true;
-};
-Private['supportedBrowser'] = supportedBrowser;
-
 /**
     Tag.getOptions returns the current options, accounting for the current
     mode if necessary.
@@ -304,13 +270,9 @@ var getOptions = function(options){
         }
         
         return options[mode];
-    } else if (SINGLE_MODE && supportedBrowser(Tag['ua'])) {
-        // If there is no mode set, return the options object if the browser is
-        // supported.
-        return options;
     }
 
-    return;
+    return options;
 };
 Tag['getOptions'] = getOptions;
 
