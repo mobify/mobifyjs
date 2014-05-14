e96ea7d15faf874cbb4abffef71988b385d53d63
Noah Adams <noah.adams@gmail.com>
2014-05-14 16:22:17 -0700

    some fixes for style, make argument be used and fix regex

--- src/patchAnchorLinks.js
+++ src/patchAnchorLinks.js
@@ -11,14 +11,14 @@
         module.exports = factory(Utils);
     } else {
         // Browser globals (root is window)
-        root.Jazzcat = factory(root.Utils);
+        root.Utils = factory(root.Utils);
     }
 }(this, function (Utils) {
     var exports = {};
 
     var isOldFirefox = function(ua) {
-        ua = window.navigator.userAgent;
-        var match = /(firefox|fennec)[\/\s](\d+)/i.exec(ua);
+        ua = ua || window.navigator.userAgent;
+        var match = /(firefox|fennec)[\/](\d+)/i.exec(ua);
         if (match) {
             var version = +match[2];
             if (version >= 29) {
@@ -54,7 +54,7 @@
 
             var matches = function(el) {
                 return (el.nodeName == "A") && (/^#/.test(el.getAttribute('href')));
-            }
+            };
 
             if (!matches(target)) {
                 return;
@@ -81,7 +81,7 @@
                 e.preventDefault = function() {
                     e.defaultPrevented = true;
                     scroll = false;
-                }
+                };
 
                 // If no other events call `preventDefault` we manually
                 // scroll to the element in question.
@@ -90,7 +90,7 @@
                         _scrollToAnchor(target.getAttribute('href'));
                     }
                 }, 50);
-            }   
+            }
         };
 
 
@@ -106,7 +106,7 @@
             if (match && match[1] === "") {
                 target = doc.body;
             } else if (match && match[1]) {
-                var target = doc.getElementById(match[1]);
+                target = doc.getElementById(match[1]);
             }
 
             // Scroll to it, if it exists
@@ -123,11 +123,14 @@
 
     var patchAnchorLinks = function() {
         if (!isOldFirefox()) {
-            return
+            return;
         }
 
         Utils.waitForReady(document, _patchAnchorLinks);
-    }
+    };
+
+    patchAnchorLinks._isOldFirefox = isOldFirefox;
+
 
     return patchAnchorLinks;
 }));
